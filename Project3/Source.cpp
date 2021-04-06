215d267d1353efbbe5239f700f8ddd00441d119f
mtiik <matvey@samohin.pro>
2021-04-07 01:21:19 +0300

    refactor code

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -5,99 +5,90 @@ using namespace std;
 
 const char symbols[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const int symbolsSize = 52;
+int len = 0;
+int strCounter = 0;
 
-bool isAlphaSymb(char str) {
-	int size = static_cast<int>(strlen(str));
-	bool flag = false;
-	for (int i = 0; i < size; ++i){
-		for(int j = 0; j < symbolsSize; j++) {
-			if (str[i] == symbols[j])
-				flag = true;
+int isAlphaSymb(char str) {
+	for (int i = 0; i < symbolsSize; ++i) {
+		if (*&(str) == *(symbols + i)) {
+			return true;
 		}
-		if (!flag)
-			return 0;
-		else
-			flag = false;
 	}
-	return true;
+	
+	return false;
 }
 
-int* searchFirstAlphaSubstr(char* str, int* len) {
-	void* addr;
+char* searchFirstAlphaSubstr(char* str, int& len) {
+	int cnt = 0;
+	char* pntr;
 	for (int i = 0; i < static_cast<int>(strlen(str)); ++i) {
-		if (isAlphaSymb(str[i])) {
-			for (int j = i; j < static_cast<int>(strlen(str)); ++j) {
-				if (isAlphaSymb(str[j])) {
-					++*len;
+		if (cnt >= strCounter) {
+			if (isAlphaSymb(*(str + i))) {
+				for (int j = i; j < static_cast<int>(strlen(str)); ++j) {
+					if (!isAlphaSymb(*(str + j))) {
+						break;
+					}
+					++len;
 				}
+				strCounter++;
+				pntr = *(&str + i);
+				return pntr;
 			}
-			addr = &str[i];
-			return static_cast <int*> (addr);
-			break;
 		}
+		++cnt;
 	}
 	return 0;
 }
 
 void delNfirstSymb(char* str, int n) {
-	for (int i = 0; i < n; ++i) {
-		for (int j = 1; j <= static_cast<int>(strlen(str)); ++j) {
-			str[j - 1] = str[j];
-		}
+	for (int i = n-1; i >= 0; --i) {
+		*(str + i + 1) = *(str+i);
 	}
 }
 
-void insertNfirstSymb(char* str, char* symb, int n) {
+void insertNfirstSymb(char* str, char symb, int n) {
 	int bufer = 0;
+	char* pointer;
+	pointer = searchFirstAlphaSubstr(str, len);
+	bufer = static_cast<int>(strlen(pointer));
 	for (int j = 0; j < n; ++j) {
-		bufer = static_cast<int>(strlen(str));
-		for (int i = bufer; i > 0; --i) {
+		for (int i = static_cast <int> (strlen(pointer)); i > 0; --i) {
 			if (i != 1) {
-				str[i] = str[i - 1];
+				*(pointer + i) = *(pointer + i - 1);
 			}
 			else {
-				str[i] = str[i - 1];
-				str[i - 1] = symb[0];
-				str[bufer + 1] = '\0';
+				*(pointer + i) = *(pointer + i - 1);
+				*(pointer + i - 1) = *&(symb);
+				*(pointer + bufer + 1) = '\0';
 			}
 			
 		}
 	}
 }
 
-char* insertSymbAfterAlpha(char* str, char* symb) {
-	int* symbAdr;
-	char strPiece[1000];
-	int len = 0, k = 0;
-	bool flag = false;
-	for (int i = 0; i < static_cast<int>(strlen(str)); ++i) {
-		symbAdr = searchFirstAlphaSubstr(str, &len);
-		if (flag) {
+char* vstavkaSymbPosleBukv(char* str, char symb) {
+	int counter = 0;
+	char* pntr;
+	if (!isAlphaSymb(*(str)))
+		return 0;
+	while (true) {
+		if (counter % 2 == 0) {
 			insertNfirstSymb(str, symb, 1);
-			flag = false;
 		}
 		else {
-			for (int i = 0; i < static_cast<int>(strlen(str)); ++i) {
-				if (static_cast<int*>((void*)str[i]) == symbAdr) {
-					for (int j = 0; j < len; j++) {
-						strPiece[j] = str[i + k];
-
-						++k;
-					}
-					break;
-				}
-			}
-			delNfirstSymb(strPiece, len);
-
-			flag = true;
+			pntr = searchFirstAlphaSubstr(str, len);
+			delNfirstSymb(pntr, len);
 		}
+		counter++;
 	}
+	return (&*(str));
 }
 
 int main(int argc, char* argv[]) {
 	char str[1000];
 	char symb[1000];
-	int n, len = 0;
+	char* uk;
+	int i = 0;
 
 	cout << "Ââåäèòå ñòðîêó: ";
 	cin.getline(str, 1000, '\n');
@@ -105,13 +96,13 @@ int main(int argc, char* argv[]) {
 	cout << "Ââåäèòå ñèìâîë: ";
 	cin.getline(symb, 1000, '\n');
 
-	cin >> n;
+	uk = vstavkaSymbPosleBukv(str, *symb);
 
-	insertNfirstSymb(str, symb, n);
-
-	cout << str;
-	
-	int* symbAdr = searchFirstAlphaSubstr(str, &len);
+	while (*(uk + i)) {
+		cout << *(uk + i) << " ";
+		++i;
+	}
+	cout << '\n';
 
 	system("PAUSE");
 	return 0;
